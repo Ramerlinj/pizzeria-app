1031a82f0ae730cdccc9f2be61c6bd81ba3c42d5
Ramerlinj <ramerlint2008@gmail.com>
2025-12-31 19:51:00 -0400

    feat: add Hero component for enhanced visual appeal and user interaction

--- app/(home)/_components/Hero.tsx
+++ app/(home)/_components/Hero.tsx
@@ -8,13 +8,13 @@ export const Hero = () => {
       <div className="container mx-auto max-w-full h-svh overflow-hidden curva">
         <Image
           src="/banners/pizza-cheese.png"
-          alt="Description"
+          alt="imagen pizza "
           width={1920}
           height={1080}
-          className="object-cover w-full h-full"
+          className="object-cover w-full h-full pointer-events-none"
         />
       </div>
-      <div className="absolute flex flex-col bottom-0 z-50 left-1/2 transform-3d transform -translate-x-1/2 translate-y-1/2 ">
+      <div className="absolute flex flex-col bottom-0 z-40 left-1/2 transform-3d transform -translate-x-1/2 translate-y-1/2 ">
         <div className="bg-linear-to-b from-transparent from-49% to-amber-600 to-40% -translate-z-1 -z-10 rounded-full p-2 shadow-xl">
           <div className="rounded-full relative flex flex-col items-center justify-center bg-white w-40 h-40">
             <Button

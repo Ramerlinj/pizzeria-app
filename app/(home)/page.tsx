89c71a9229dcc9369378cf0144547a5f4cbcaed1
Ramerlinj <ramerlint2008@gmail.com>
2025-12-31 19:51:12 -0400

    feat: update Home component layout and add pizza data interface

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,15 @@
-import { Hero } from "./_components/hero";
+import { Hero } from "./_components/Hero";
+import { AboutPizza } from "./_components/AboutPizza";
+import { PopularPizza } from "./_components/PopularPizza";
+import { Categories } from "./_components/Categories";
 
 export default function Home() {
   return (
-   <main>
-    <Hero />
-   </main>
+    <main className="min-h-screen">
+      <Hero />
+      <Categories />
+      <AboutPizza />
+      <PopularPizza />
+    </main>
   );
 }
